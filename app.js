938a9af75da79de8bbfe0a3d342bdfed1830aafa
MohamedSamy <Mohamed.Samy.K@gmail.com>
2015-07-12 07:11:24 +0200

    Directives

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var metaInfos = angular.module('metaInfos', []);
 
 metaInfos.controller('UserCtrl', ['$scope', function($scope){
+    $scope.names = ['Mohamed', 'Kareem', 'Ahmed'];
     $scope.name = 'Anonymous';
+    $scope.addName = function(name){
+        $scope.names.push(name);
+    }
 }]);
